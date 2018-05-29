5e2eb9bc6c96104e7f70d24b2cbd88de833691de
ben-hurfidalgo <ben-hurfidalgo@192.168.1.104>
2018-05-29 19:10:01 +0100

    - Rececao dos dados PAHO:JAVA 100% - Entrega dos dados JAVA:MONGODB 100% - Migracao dos dados MONGODB:SYBASE por testar

--- J_App/src/JApp.java
+++ J_App/src/JApp.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import com.mongodb.BasicDBObject;
 import com.mongodb.DB;
 import com.mongodb.DBCollection;
+import com.mongodb.DBCursor;
 import com.mongodb.DBObject;
 import com.mongodb.MongoClient;
 
@@ -14,7 +15,7 @@ public class JApp {
 	private MongoClient client;
 	private DB db;
 	private List<Sensor> sensores = new ArrayList<Sensor>();
-	private List<String[]> mongo_list = new ArrayList<String[]>();
+	private List<String[]> mongo_list = new ArrayList<String[]>(), migration_list;
 	protected long java_mongo_sleep = 15 * 1000;
 
 	protected JApp() {
@@ -49,26 +50,29 @@ public class JApp {
 			DBCollection collection = db.getCollection("humidtemp_aux");
 
 			/*
-			 * Thread responsavel por transferir todos os dados que estejam em espera na lista do java para o mongo=>Collection=>humidtemp_aux
+			 * Thread responsavel por transferir todos os dados que estejam em espera na
+			 * lista do java para o mongo=>Collection=>humidtemp_aux
 			 */
 			new Thread(new Runnable() {
 				@Override
 				public void run() {
 					while (true) {
-						
+
 						for (String[] vector_info : mongo_list) {
 							String temperature = vector_info[0];
 							String humidity = vector_info[1];
 							String date = vector_info[2];
 							String time = vector_info[3];
-							
-							DBObject sensor_data = new BasicDBObject("temperature", temperature).append("humidity", humidity)
-									.append("date", date).append("time", time);
+
+							DBObject sensor_data = new BasicDBObject("temperature", temperature)
+									.append("humidity", humidity).append("date", date).append("time", time);
 							DBCollection collection = db.getCollection("humidtemp_aux");
 							collection.insert(sensor_data);
 						}
-						
-						System.out.println("INSERI "+mongo_list.size()+" entradas no MONGO \n\n");
+
+						System.out.println("INSERI " + mongo_list.size() + " entradas no MONGO \n\n");
+
+						mongo_list.clear();
 						// esperar por mais dados
 						try {
 							Thread.currentThread().sleep(java_mongo_sleep);
@@ -89,7 +93,8 @@ public class JApp {
 	}
 
 	private void startSybaseLink() {
-
+		Sybase_connection sybase_connection = new Sybase_connection();
+		sybase_connection.start();
 	}
 
 	// https://eclipse.org/paho/clients/js/utility/
@@ -115,9 +120,55 @@ public class JApp {
 				vector_info[3] = time;
 
 				mongo_list.add(vector_info);
-				
+
 				System.out.println("COLOQUEI DADOS NA LISTA DO JAVA em espera para o MONGO \n\n");
 			}
 		}).start();
+	}
+
+	public List<String[]> getMigrationData() {
+		DBCollection collection = db.getCollection("humidtemp_aux");
+		DBCursor cursor = collection.find();
+		migration_list = new ArrayList<String[]>();
+		
+		while (cursor.hasNext()) {
+			DBObject obj = cursor.next();
+			String temperature = (String) obj.get("temperature");
+			String humidity = (String) obj.get("humidity");
+			String date = (String) obj.get("date");
+			String time = (String) obj.get("time");
+			
+			String[] vector_info = new String[4];
+
+			vector_info[0] = temperature;
+			vector_info[1] = humidity;
+			vector_info[2] = date;
+			vector_info[3] = time;
+
+			migration_list.add(vector_info);			
+		}
+		return migration_list;
+	}
+
+	public void mongodbCollectionDataTransfer() {
+		// obter dados a tranferir
+		DBCollection auxColl = db.getCollection("humidtemp_aux");
+		// criar lista em formato compativel com os dados
+		List<DBObject> object_list = new ArrayList<DBObject>();
+		// obter cursor associado a colecao
+		DBCursor cursor = auxColl.find();
+		// percorrer colecao copiando os dados
+		while (cursor.hasNext()) {
+			DBObject dbObject = cursor.next();
+			object_list.add(dbObject);
+		}
+		// obter colecao permanente dos dados na mongodb
+		DBCollection perm_coll = db.getCollection("humidtemp");
+		// inserir dados da lista na colecao permanente
+		perm_coll.insert(object_list);
+		// eliminar colecao temporaria dos dados
+		auxColl.drop();
+		// recriar colecao temporaria para uso futuro
+		db.getCollection("humidtemp_aux");
 	}
 }
--- J_App/src/Sybase_connection.java
+++ J_App/src/Sybase_connection.java
@@ -2,36 +2,69 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
+import java.util.List;
+
 import sybase.jdbc4.sqlanywhere.*;
 
 public class Sybase_connection {
-	//private Sensor s;
+	// private Sensor s;
 	private String time;
 	private Connection connect;
 	private Statement stmn;
 	private ResultSet rs;
-	
+
 	public void start() {
-		int temp=5;
-		int hum=4;
+		int temp = 5;
+		int hum = 4;
 		try {
 			System.out.println("vou ligar");
-			connect=DriverManager.getConnection("jdbc:sqlanywhere:uid=DBA;pwd=sql;eng=SID_DB2-1;database=SID_DB2-1");
+			connect = DriverManager.getConnection("jdbc:sqlanywhere:uid=DBA;pwd=sql;eng=SID_DB2-1;database=SID_DB2-1");
 			System.out.println("j· liguei");
-			stmn =connect.createStatement();
-			rs=stmn.executeQuery("INSERT into HumidadeTemperatura(DataMed, HoraMed, ValorMedTemp, ValorMedHum, IDmedicao) VALUES (NULL, NULL,"+ temp +","+hum+",(select count(IDmedicao)+1 from HumidadeTemperatura))");
-			System.out.println("criei");
-			/*
-			 * CONFIGURAR AQUI O INSERT STATEMENT PARA OS DADOS QUE VEM DO MONGO PARA UMA LISTA DO JAVA: Sybase_List
-			 */
-			/*for(int i=0;i<3;i++) {
-			rs.next();
-			System.out.println(rs.getString(3));
-			}*/
-		}catch(Exception e) {
+			stmn = connect.createStatement();
+			// obter id da proxima alinea para insercao
+			ResultSet rs=stmn.executeQuery("select count(IDmedicao)+1 from HumidadeTemperatura");
+			int queryIt =  ((Number) rs.getObject(1)).intValue();
+			// preparar o query da migracao de dados do mongodb para o sybase
+			String queryMigration = createQuery(queryIt);
+			rs = stmn.executeQuery(queryMigration);
+			System.out.println("migrei os dados");
+			// mover os dados da colecao temporaria para a permanente no mongodb
+			JApp.getInstance().mongodbCollectionDataTransfer();
+		} catch (Exception e) {
 			System.out.println("SYBASE OFF");
 		}
 	}
+
+	/*
+	 * INSERT INTO MyTable ( Column1, Column2, Column3 ) VALUES ('John', 123,
+	 * 'Lloyds Office'), ('Jane', 124, 'Lloyds Office'), ('Billy', 125, 'London
+	 * Office'), ('Miranda', 126, 'Bristol Office');
+	 */
+	private String createQuery(int nextId) {
+		int idLocal = nextId;
+		// obter lista com dados a inserir no sybase
+		List<String[]> migrationData = JApp.getInstance().getMigrationData();
+		// criar uma variavel para cada coluna
+		String localQuery = "INSERT into HumidadeTemperatura(DataMed, HoraMed, ValorMedTemp, ValorMedHum, IDmedicao) VALUES ";
+		// iniciar a 1 porque o size começa a 1, caso nao esteja vazia
+		int counter = 1;
+		int list_size = migrationData.size();
+		// percorrer a lista de dados afim de elaborar as variaveis com os dados das
+		for (String[] data : migrationData) {
+			
+			String content = "("+ data[0] +", "+ data[1] +", "+ data[2] +", "+ data[3] +", "+idLocal+")";
+			idLocal++;
+			
+			localQuery += content;
+			// preparar para proxima entrada caso nao seja o ultimo
+			if(counter < list_size) {
+				localQuery += ", ";
+			}
+			counter++;
+		}				
+		return localQuery;
+	}
+
 	public static void main(String[] args) {
 		Sybase_connection db = new Sybase_connection();
 		db.start();
